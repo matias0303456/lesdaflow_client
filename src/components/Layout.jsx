bf5b4af00362cb4586ecfc1d30494e4effcec534
Matias Ramos <matias.ramos@newbrick.co>
2024-03-21 11:49:44 -0300

    avences

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,47 +1,21 @@
 import { useContext, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import { Dropdown } from '@mui/base/Dropdown';
-import { Menu } from '@mui/base/Menu';
-import { MenuButton as BaseMenuButton } from '@mui/base/MenuButton';
-import { MenuItem as BaseMenuItem, menuItemClasses } from '@mui/base/MenuItem';
-import { styled } from '@mui/system';
-import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import InputSharpIcon from '@mui/icons-material/InputSharp';
-import OutputSharpIcon from '@mui/icons-material/OutputSharp';
-import MonetizationOnSharpIcon from '@mui/icons-material/MonetizationOnSharp';
-import ProductionQuantityLimitsSharpIcon from '@mui/icons-material/ProductionQuantityLimitsSharp';
-import Person2SharpIcon from '@mui/icons-material/Person2Sharp';
-import LocalShippingSharpIcon from '@mui/icons-material/LocalShippingSharp';
-import ManageAccountsSharpIcon from '@mui/icons-material/ManageAccountsSharp';
-import PermContactCalendarSharpIcon from '@mui/icons-material/PermContactCalendarSharp';
-import LogoutSharpIcon from '@mui/icons-material/LogoutSharp';
-import AssessmentSharpIcon from '@mui/icons-material/AssessmentSharp';
-import AccountBalanceWalletSharpIcon from '@mui/icons-material/AccountBalanceWalletSharp';
 
 import { AuthContext } from '../providers/AuthProvider';
 
 import Logo from '../assets/logo.jpeg'
 
 export function Layout({ children, title }) {
-
     const { auth, setAuth } = useContext(AuthContext)
-
     const navigate = useNavigate()
     const { pathname } = useLocation()
-
     const [mobileOpen, setMobileOpen] = useState(false)
+    const [submenu, setSubmenu] = useState(null)
 
     const drawerWidth = 240
 
@@ -56,56 +30,72 @@ export function Layout({ children, title }) {
     return (
         <Box sx={{
             display: 'flex',
-            justifyContent: 'space-between',
-            backgroundColor: '#078BCD',
-            padding: 1
+            flexDirection: 'column', // Change to column layout
+            height: '100vh' // Make sure the container spans the full viewport height
         }}>
-            <Box sx={{ width: '70%', display: 'flex', gap: 3 }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', width: '10%' }}>
-                    <img src={Logo} width={80} />
+            <Box sx={{ // Top bar
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                backgroundColor: '#078BCD',
+                padding: 1
+            }}>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    sx={{ mr: 2, display: { md: 'none' }, color: '#fff' }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Box sx={{ width: '70%', display: 'flex', gap: 3 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', width: '10%' }}>
+                        <img src={Logo} width={80} />
+                    </Box>
+                    <Box sx={{ display: { xs: 'none', md: 'flex' }, justifyContent: 'space-between', width: '90%' }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onMouseEnter={() => setSubmenu('clients')} onMouseLeave={() => setSubmenu(null)}>
+                            <Typography variant='p' color="#fff" sx={{ position: 'relative' }}>
+                                Clientes <span style={{ fontSize: '.7rem' }}>▼</span>
+                                <Box sx={{ position: 'absolute', top: '100%', left: '0', display: submenu === 'clients' ? 'block' : 'none', background: '#fff', padding: '10px', zIndex: 2 }}>
+                                    <ul style={{ listStyle: 'none' }}>
+                                        <li>ABM Clientes</li>
+                                        <li>Visitas</li>
+                                        <li>Cuentas Corrientes</li>
+                                        <li>Reporte Cuenta Corriente</li>
+                                    </ul>
+                                </Box>
+                            </Typography>
+                        </Box>
+
+                    </Box>
                 </Box>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '90%' }}>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography variant='p' color="#fff">
-                            Clientes
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography variant='p' color="#fff">
-                            Vendedor
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography variant='p' color="#fff">
-                            Vendedor
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography variant='p' color="#fff">
-                            Ventas
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography variant='p' color="#fff">
-                            Productos
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography variant='p' color="#fff">
-                            Presupuestos
-                        </Typography>
-                    </Box>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Typography variant='p' color="#fff">
-                            Cajas
-                        </Typography>
-                    </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', width: '30%', justifyContent: 'end' }}>
+                    <Typography variant='p' color="#fff">
+                        {auth.user.username} <span style={{ fontSize: '.7rem' }}>▼</span>
+                    </Typography>
                 </Box>
             </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center', width: '30%', justifyContent: 'end' }}>
-                <Typography variant='p' color="#fff">
-                    {auth.user.username}
-                </Typography>
+            <Drawer
+                anchor="left"
+                open={mobileOpen}
+                onClose={handleDrawerToggle}
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+            >
+                <div>
+                    {/* Your drawer content here */}
+                </div>
+            </Drawer>
+            <Box sx={{ flex: 1, overflow: 'auto' }}>
+                {/* Main content */}
+                {children}
             </Box>
         </Box>
     );
