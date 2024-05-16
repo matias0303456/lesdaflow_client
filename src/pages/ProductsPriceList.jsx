8b9a0632e1abbca78560a78b043008cf7961678d
matias ramos <matiasramos261@gmail.com>
2024-05-16 17:51:28 -0300

    lista de precios

--- src/pages/ProductsPriceList.jsx
+++ src/pages/ProductsPriceList.jsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Box,
   Button,
@@ -8,246 +9,141 @@ import {
   LinearProgress,
   Typography,
 } from "@mui/material";
-import { CLIENT_URL, USER_URL } from "../utils/urls";
 
 import { AuthContext } from "../providers/AuthProvider";
-import { MessageContext } from "../providers/MessageProvider";
-import { useApi } from "../hooks/useApi";
-import { useForm } from "../hooks/useForm";
-import { useClients } from "../hooks/useClients";
+import { useProducts } from "../hooks/useProducts";
 
 import { Layout } from "../components/Layout";
-
 import { DataGrid } from "../components/DataGrid";
 
+import { getStock } from "../utils/helpers";
+import { useForm } from "../hooks/useForm";
 
 export function ProductsPriceList() {
+
   const { auth } = useContext(AuthContext);
-  const { setMessage, setOpenMessage, setSeverity } =
-    useContext(MessageContext);
-  // clients import
-  const { get, post, put } = useApi(CLIENT_URL);
-  const { clients, setClients, loadingClients, setLoadingClients } =
-    useClients();
-  // users import
-  const { get: getUsers } = useApi(USER_URL);
 
-  const {
-    formData,
-    setFormData,
-    handleChange,
-    disabled,
-    setDisabled,
-    validate,
-    reset,
-    errors,
-  } = useForm({
-    defaultData: {},
-    rules: {},
-  });
+  const navigate = useNavigate()
 
-  const [open, setOpen] = useState(null);
-  const [loadingUsers, setLoadingUsers] = useState(true);
-  const [users, setUsers] = useState([]);
+  const { products, loadingProducts } = useProducts()
+  const { formData, handleChange } = useForm({ defaultData: { code: '', details: '' } })
 
   useEffect(() => {
-    if (auth?.user.role.name !== "ADMINISTRADOR") navigate("/productos");
+    if (auth?.user.role !== "ADMINISTRADOR") navigate("/productos");
   }, []);
-
-  useEffect(() => {
-    (async () => {
-      const { status, data } = await getUsers();
-      if (status === 200) {
-        setUsers(data);
-        setLoadingUsers(false);
-      }
-    })();
-  }, []);
-
-  useEffect(() => {
-    (async () => {
-      const { status, data } = await get();
-      if (status === 200) {
-        setClients(data);
-        setLoadingClients(false);
-      }
-    })();
-  }, []);
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if (validate()) {
-      const { status, data } =
-        open === "NEW" ? await post(formData) : await put(formData);
-      if (status === 200) {
-        if (open === "NEW") {
-          setClients([data, ...clients]);
-          setMessage("Cliente creado correctamente.");
-        } else {
-          setClients([data, ...clients.filter((c) => c.id !== formData.id)]);
-          setMessage("Cliente editado correctamente.");
-        }
-        setSeverity("success");
-        reset(setOpen);
-      } else {
-        setMessage(data.message);
-        setSeverity("error");
-        setDisabled(false);
-      }
-      setOpenMessage(true);
-    }
-  }
 
   const headCells = [
     {
-        id: 'cod',
-        numeric: false,
-        disablePadding: true,
-        label: 'Cod.',
-        accessor: 'cod',
+      id: 'code',
+      numeric: false,
+      disablePadding: true,
+      label: 'Cod.',
+      accessor: 'code',
     },
     {
-        id: 'product',
-        numeric: false,
-        disablePadding: true,
-        label: 'Producto',
-        accessor: 'product'
+      id: 'details',
+      numeric: false,
+      disablePadding: true,
+      label: 'Producto',
+      accessor: 'details'
     },
     {
-        id: 'sale_price',
-        numeric: false,
-        disablePadding: true,
-        label: 'Precio Venta',
-        accessor: 'sale_price'
+      id: 'sale_price',
+      numeric: false,
+      disablePadding: true,
+      label: 'Precio de venta',
+      sorter: (row) => parseFloat((row.buy_price + ((row.buy_price / 100) * row.earn)).toFixed(2)),
+      accessor: (row) => `$${(row.buy_price + ((row.buy_price / 100) * row.earn)).toFixed(2)}`
     },
     {
-        id: 'stock',
-        numeric: false,
-        disablePadding: true,
-        label: 'Stock',
-        accessor: 'stock'
-    },
-]
+      id: 'stock',
+      numeric: false,
+      disablePadding: true,
+      label: 'Stock',
+      sorter: (row) => getStock(row),
+      accessor: (row) => getStock(row)
+    }
+  ]
 
   return (
     <Layout title="Lista de Precios">
-    {/* upper section */}
-      <Box className="w-[100%] mb-3 px-2 py-4 bg-white rounded-md">
-        <Typography
-          variant="h6"
-          sx={{
-            width: "100%",
-            fontSize: "14px",
-            color: "white",
-            paddingX: "10px",
-            paddingY: "5px",
-            backgroundColor: "#078BCD",
-            borderRadius: "2px",
-            fontWeight: "bold",
-          }}
-        >
-          Productos
-        </Typography>
-
-        {loadingClients || loadingUsers ? (
-          <Box sx={{ width: "100%" }}>
-            <LinearProgress />
-          </Box>
-        ) : (
-          <form onChange={handleChange} onSubmit={handleSubmit}>
-
-             <Box 
-        sx={{ display: "flex", alignItems: "center", justifyContent: "start", gap: 1 }}>
-        
-                  {/* product cod */}
-            <FormControl variant="standard" sx={{
-                    width: "20%",
-                    color: "#59656b",
-                    display: "flex",                    alignItems: "start",
-                    justifyContent: "center",
-                    marginTop: "2rem"
-                }}>
-                <InputLabel id="demo-simple-select-standard-label">
-                  Cód.
-                </InputLabel>
-               <Input/>
-              </FormControl>
-                {/* product name */}
-              <FormControl variant="standard" sx={{
-                    width: "100%",
-                    color: "#59656b",
-                    display: "flex",                    alignItems: "start",
-                    justifyContent: "center",
-                    marginTop: "2rem"
-                }}>
-                <InputLabel id="demo-simple-select-standard-label">
-                  Producto
-                </InputLabel>
-               <Input
-               sx={{
+      {loadingProducts ?
+        <Box sx={{ width: "100%" }}><LinearProgress /></Box> :
+        <>
+          <Box className="w-[100%] mb-3 px-2 py-4 bg-white rounded-md">
+            <Typography
+              variant="h6"
+              sx={{
                 width: "100%",
-               }}/>
-              </FormControl>
-            </Box>
-
-          </form>
-        )}
-      </Box>
-
-      {/* down section */}
-      <Box className="w-[100%] px-2 py-4 bg-white rounded-md">
-        <Typography
-          variant="h6"
-          sx={{
-            width: "100%",
-            fontSize: "14px",
-            color: "white",
-            paddingX: "10px",
-            paddingY: "5px",
-            backgroundColor: "#078BCD",
-            borderRadius: "2px",
-            fontWeight: "bold",
-          }}
-        >
-          Precios
-        </Typography>
-
-        {loadingClients || loadingUsers ? (
-          <Box sx={{ width: "100%" }}>
-            <LinearProgress />
+                fontSize: "14px",
+                color: "white",
+                paddingX: "10px",
+                paddingY: "5px",
+                backgroundColor: "#078BCD",
+                borderRadius: "2px",
+                fontWeight: "bold",
+              }}
+            >
+              Productos
+            </Typography>
+            <form onChange={handleChange}>
+              <Box
+                sx={{ display: "flex", alignItems: "center", justifyContent: "start", gap: 1 }}>
+                <FormControl variant="standard" sx={{
+                  width: "20%",
+                  color: "#59656b",
+                  display: "flex", alignItems: "start",
+                  justifyContent: "center",
+                  marginTop: "2rem"
+                }}>
+                  <InputLabel id="demo-simple-select-standard-label">
+                    Cód.
+                  </InputLabel>
+                  <Input name="code" />
+                </FormControl>
+                <FormControl variant="standard" sx={{
+                  width: "100%",
+                  color: "#59656b",
+                  display: "flex", alignItems: "start",
+                  justifyContent: "center",
+                  marginTop: "2rem"
+                }}>
+                  <InputLabel id="demo-simple-select-standard-label">
+                    Producto
+                  </InputLabel>
+                  <Input
+                    sx={{ width: "100%" }} name="details" />
+                </FormControl>
+              </Box>
+            </form>
           </Box>
-        ) : (
-         <Box className="w-[50%]">
-          <DataGrid
-             headCells={headCells}
-             rows={clients}
-             setOpen={setOpen}
-             setData={setFormData}
-             contentHeader={
-               <Box
-                 sx={{
-                   display: "flex",
-                   flexWrap: "wrap",
-                   alignItems: "center",
-                   gap: 2,
-                 }}
-               >
-                
-                 {/* user filter implementation */}
-                 {/* <UserFilter users={users} setUsers={setUsers} /> */}
-                 {/* by search box approach  */}
-                 
-               </Box>
-             }
-          >
-
-          </DataGrid>
-          <Button variant="outlined" size="medium" color="info">
-            Salir
-          </Button>
-         </Box>
-        )}
-      </Box>
+          <Box className="w-[100%] px-2 py-4 bg-white rounded-md">
+            <Typography
+              variant="h6"
+              sx={{
+                width: "100%",
+                fontSize: "14px",
+                color: "white",
+                paddingX: "10px",
+                paddingY: "5px",
+                backgroundColor: "#078BCD",
+                borderRadius: "2px",
+                fontWeight: "bold",
+              }}
+            >
+              Precios
+            </Typography>
+            <DataGrid
+              headCells={headCells}
+              rows={products.filter(p => (formData.code.length === 0 || p.code.includes(formData.code)) &&
+                (formData.details.length === 0 || p.details.includes(formData.details)))}
+            />
+            <Button variant="outlined" size="medium" color="info" onClick={() => navigate('/productos')}>
+              Salir
+            </Button>
+          </Box>
+        </>
+      }
     </Layout>
   );
 }
