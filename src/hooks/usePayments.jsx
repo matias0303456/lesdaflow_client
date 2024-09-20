d4ae9e92bea9d97fae0192c1a445dc5e73162175
matias ramos <matiasramos261@gmail.com>
2024-09-20 11:36:57 -0300

    alta de pago

--- src/hooks/usePayments.jsx
+++ src/hooks/usePayments.jsx
@@ -4,7 +4,6 @@ import { MessageContext } from "../providers/MessageProvider"
 import { useQuery } from "./useQuery"
 
 import { PAYMENT_URL } from "../utils/urls"
-// import { getSaleDifference } from "../utils/helpers"
 import { STATUS_CODES } from "../utils/constants"
 
 export function usePayments() {
@@ -16,40 +15,24 @@ export function usePayments() {
 
     const { handleQuery } = useQuery()
 
-    // const checkDifference = (formData) => {
-    //     if (formData.amount.toString().length === 0 || parseFloat(formData.amount) === 0) {
-    //         setMessage('El importe es requerido.')
-    //         setSeverity('error')
-    //     } else {
-    //         const diff = getSaleDifference(state.sales.data.find(s => s.id === formData.sale_id)).replace('$', '')
-    //         if (parseFloat(diff) >= parseFloat(formData.amount)) return true
-    //         setMessage(`El importe debe ser menor al saldo. Saldo actual: $${diff}`)
-    //         setSeverity('error')
-    //     }
-    //     setOpenMessage(true)
-    //     return false
-    // }
-
     const handleSubmit = async (e, validate, formData, reset, setDisabled) => {
         e.preventDefault()
-        // if (!checkDifference(formData)) return
         if (validate()) {
-            const urls = { 'NEW': PAYMENT_URL, 'EDIT': `${PAYMENT_URL}/${formData.id}` }
-            const methods = { 'NEW': 'POST', 'EDIT': 'PUT' }
+            const urls = { 'NEW-PAYMENT': PAYMENT_URL, 'EDIT': `${PAYMENT_URL}/${formData.id}` }
+            const methods = { 'NEW-PAYMENT': 'POST', 'EDIT': 'PUT' }
             const { status, data } = await handleQuery({
                 url: urls[open],
                 method: methods[open],
                 body: formData
             })
             if (status === STATUS_CODES.OK || status === STATUS_CODES.CREATED) {
-                if (open === 'NEW') {
+                if (open === 'NEW-PAYMENT') {
                     setMessage('Pago creado correctamente.')
                 } else {
                     setMessage('Pago editado correctamente.')
                 }
                 setSeverity('success')
                 reset(setOpen)
-                return data
             } else {
                 setMessage(data.message)
                 setSeverity('error')
@@ -67,7 +50,6 @@ export function usePayments() {
         if (status === STATUS_CODES.OK) {
             setMessage('Pago eliminado correctamente.')
             setSeverity('success')
-            return data
         } else {
             setMessage(data.message)
             setSeverity('error')
