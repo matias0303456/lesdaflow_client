45f0b9e2030f2751a4c64a9399c1ba3cffde9601
matias ramos <matiasramos261@gmail.com>
2024-09-25 09:13:55 -0300

    deploy 25-9

--- src/components/commercial/SaleForm.jsx
+++ src/components/commercial/SaleForm.jsx
@@ -305,7 +305,7 @@ export function SaleForm({
                                 <Button
                                     type="submit"
                                     variant="contained"
-                                    disabled={disabled || isBlocked}
+                                    disabled={disabled || (isBlocked && open === 'NEW')}
                                     sx={{ width: '50%' }}
                                 >
                                     {confirmed ? 'Guardar' : 'Confirmar'}
