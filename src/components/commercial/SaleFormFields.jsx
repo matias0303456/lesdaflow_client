0688f185292eb82bd037b73946187f7cfd22c8ed
matias ramos <matiasramos261@gmail.com>
2025-03-28 13:30:09 -0300

    fix

--- src/components/commercial/SaleFormFields.jsx
+++ src/components/commercial/SaleFormFields.jsx
@@ -44,10 +44,6 @@ export function SaleFormFields({
         if (saleProducts.length === 0 || discountApplied?.id) setDiscountApplied('')
     }, [saleProducts])
 
-    useEffect(() => {
-        console.log(discountApplied?.base > 0 && discountApplied?.base < formData.total, discountApplied?.base, formData.total)
-    }, [formData, discountApplied])
-
     return (
         <Box sx={{ p: 1 }}>
             <form onChange={handleChange} onSubmit={(e) => {
@@ -247,7 +243,7 @@ export function SaleFormFields({
                             variant="contained"
                             disabled={
                                 disabled ||
-                                (discountApplied?.base > 0 && discountApplied?.base < formData.total) ||
+                                discountApplied?.base > parseFloat(formData.total) ||
                                 (isBlocked && (open === 'NEW' || open === 'CONVERT'))
                             }
                             sx={{ width: '50%' }}
