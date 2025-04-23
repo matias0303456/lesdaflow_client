38e74395c2031a4fac5d26a1622071bdcdcdea42
matias ramos <matiasramos261@gmail.com>
2025-04-23 15:04:13 -0300

    coment

--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -131,7 +131,7 @@ export function Budgets() {
                     setOpen={setOpen}
                     setFormData={setFormData}
                     showPDFAction={`${REPORT_URL}/presupuesto-pdf?token=${auth?.token}&id=`}
-                    showConvertToSale="Convertir a venta"
+                    // showConvertToSale="Convertir a venta"
                     filter={filter}
                     setFilter={setFilter}
                     count={count}
